cb57f556ee0e409e7ce1c55122f57e23ed7ba816
Kaw Bu <kawbu@unc.edu>
2023-10-28 12:37:07 -0400

    Basics of the game (opening the game)

--- maingame.py
+++ maingame.py
@@ -1,3 +1,34 @@
-print("hello")
+import pygame
+import sys
 
-print("hi")
+pygame.init()
+
+#Variables for the game window dimensions
+screen_w = 800
+screen_h = 600
+
+#Important color codes as variables
+background = (173, 216, 230)
+ground = (0, 100, 0)
+
+
+#Creating the game
+screen = pygame.display.set_mode((screen_w, screen_h))
+
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    
+    screen.fill(background)
+
+    create_ground = pygame.Rect(0, 500, screen_w, 100)
+    pygame.draw.rect(screen, ground, create_ground)
+
+    pygame.display.flip()
+
+
+#When Game is exited
+pygame.quit()
+sys.exit()
